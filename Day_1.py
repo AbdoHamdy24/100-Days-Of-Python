367c997bc34f2b3b96eb4491ca69b53132f455c8
AbdoHamdy24 <123699932+AbdoHamdy24@users.noreply.github.com>
2026-06-23 14:48:04 +0300

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Genarator.")
+city = input("What is your city you grew up in?\n")
+pet_name = input("What ios your pet name?\n")
+print("Your Band name is " + city.capitalize() +" " + pet_name.capitalize())
